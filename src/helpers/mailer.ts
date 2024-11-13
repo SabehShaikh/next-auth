1ccbd417b8ba5c428171d5e733d8e91e90402703
SabehShaikh <sabehshaikh201@gmail.com>
2024-11-13 18:31:09 +0500

    first commit

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,40 @@
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+
+   const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 465,
+      secure: true, // true for port 465, false for other ports
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+
+    const mailOptions = {
+      from: "sabeh@sabeh.ai",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      html: "<b>Hello world?</b>", // html body
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
